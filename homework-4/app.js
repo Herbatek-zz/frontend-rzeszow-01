4d800f74b23e73b4523d77a6e05d8c42b3c6a781
Piotr Cużytek <kenshin985@gmail.com>
2018-04-24 12:22:49 +0200

    homework-4 v0.2

--- homework-4/app.js
+++ homework-4/app.js
@@ -1,19 +1,26 @@
-function addTask() {
-    let input = document.querySelector('.input');
-    let table = document.querySelector('.table');
+document.addEventListener("DOMContentLoaded", loadLocalStorage());
 
-    let listItem = createListItem(input.value);
+function createTask() {
+    let input = document.querySelector('.input');
+    addTask(input.value);
+    saveLocalStorage(input.value);
     input.value = '';
+}
+
+function addTask(input) {
+    let listItem = createListItem(input);
+    let table = document.querySelector('.table');
     table.appendChild(listItem);
 }
 
 function createListItem(text) {
     let listItem = document.createElement('li');
-
     let button = createRemoveButton();
 
     let taskText = document.createTextNode(text);
-    listItem.appendChild(taskText);
+    let pElement = document.createElement('p');
+    pElement.appendChild(taskText);
+    listItem.appendChild(pElement);
     listItem.appendChild(button);
 
     return listItem;
@@ -21,21 +28,49 @@ function createListItem(text) {
 
 function createRemoveButton() {
     let button = document.createElement('button');
-    let textOnButton = document.createTextNode('remove');
+    let textOnButton = document.createTextNode('Remove');
     button.appendChild(textOnButton);
     button.onclick = removeTask;
     return button;
 }
 
 function removeTask() {
-    let sure = confirm('Are you sure to delete: "' + this.parentNode.firstChild.data +  '" ?');
-    if (sure)
+    let deleteTask = this.parentNode.firstChild.textContent;
+    let sure = confirm('Are you sure you want to delete: "' + deleteTask + '" ?');
+    if (sure) {
         this.parentNode.remove();
+        if (readTaskList() != null) {
+            let array = readTaskList();
+            let idDeleteTask = array.indexOf(deleteTask);
+            if (idDeleteTask != -1) {
+                array.splice(idDeleteTask, 1);
+                localStorage.setItem('taskList', JSON.stringify(array));
+                }
+        }
+    }
 }
 
-/*
-to do:
-zapisywanie w localStorage
-css
+function saveLocalStorage(input) {
+    if (readTaskList() != null) {
+        let array = readTaskList();
+        console.log(input);
+        array.push(input);
+        localStorage.setItem('taskList', JSON.stringify(array));
+    } else {
+        let array = [input];
+        localStorage.setItem('taskList', JSON.stringify(array));
+    }
+}
 
-*/
+function loadLocalStorage() {
+    if (readTaskList() !== null) {
+        let array = readTaskList();
+        array.forEach(function(task) {
+            addTask(task);
+        });
+    }
+}
+
+function readTaskList() {
+    return JSON.parse(localStorage.getItem('taskList'));
+}
